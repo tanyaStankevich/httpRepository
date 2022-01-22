d3921415c3938b442be20d0c3d6dbd53009425f0
unknown <tanjastankevich@rambler.ru>
2022-01-22 20:57:42 +0300

    hometask2

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const path = require('path');
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = (req, res) => {
+const user = {
+    id: 123,
+    username: 'testuser',
+    password: 'qwerty'
+};
+
+const requestListener = (req, res) => { 
     
     if (req.url === '/get' && req.method ==='GET') {
         try {
@@ -18,13 +24,46 @@ const requestListener = (req, res) => {
     }
 
     else if (req.url === '/delete' && req.method ==='DELETE') {
-        res.writeHead(200);
-        res.end('success');
+        if (req.cookie === 'authorized = true' && req.cookie === 'userID=123') {
+            try {
+                res.writeHead(200);
+                res.end('success');
+                const filepath = path.join(__dirname, req.filename);
+                fs.unlink(filepath);
+            } catch (err){
+                res.writeHead(500);
+                res.end('Internal server error');
+             }
+        } 
     }
 
     else if (req.url === '/post' && req.method ==='POST') {
-        res.writeHead(200);
-        res.end('success');
+        
+        if (req.cookie === 'authorized = true' && req.cookie === 'userID=123') {
+            try {
+                res.writeHead(200);
+                res.end('success');
+                const createFile = fs.writeFile(req.filename, req.content);
+            } catch (err) {
+                res.writeHead(500);
+                res.end('Internal server error');
+            }   
+       }
+        else if (req.username === user.username && req.password=== user.password) {
+            res.writeHead(200, {
+                'Set-Cookie': userID = user.id,
+                'Set-Cookie': authorized = true,
+                'Set-Cookie': MAX_AGE=60 * 60 * 24 * 2,
+                'Set-Cookie': path='/'
+            });
+            res.end('success');
+        } else {
+            res.writeHead(400);
+            res.end('Неверный логин или пароль');
+        }
+        // const filePath = path.join(__dirname, 'index.html');
+        // const readStream = fs.createReadStream(filePath);
+        // readStream.pipe(response);  
     }
 
     else if (req.url === '/redirect' && req.method ==='GET') { 
